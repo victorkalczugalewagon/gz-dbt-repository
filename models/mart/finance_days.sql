c84b1e7ea531913642b03da41537fd46060cf1e8
victorkalczugalewagon@gmail.com <victorkalczugalewagon@gmail.com>
2024-02-07 13:14:58 +0000

    ok

--- models/mart/finance_days.sql
+++ models/mart/finance_days.sql
@@ -8,7 +8,7 @@ SELECT
     ,ROUND(SUM(operational_margin),0) AS operational_margin 
     ,ROUND(SUM(purchase_cost),0) AS purchase_cost 
     ,ROUND(SUM(shipping_fee),0) AS shipping_fee 
-    ,ROUND(SUM(logcost),0) AS logcost 
+    ,ROUND(SUM(log_cost),0) AS logcost 
     ,ROUND(SUM(ship_cost),0) AS ship_cost 
     ,SUM(quantity) AS quantity 
 FROM {{ref("int_orders_operational")}}
